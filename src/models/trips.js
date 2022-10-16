52d1ac96534af2e9e21e1b87552d5a47d5a1750b
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-16 00:42:23 -0500

    Add: trips model

---
+++ src/models/trips.js
@@ -0,0 +1,37 @@
+module.exports = (sequelize, DataType) => {
+  sequelize.define("Trips", {
+    id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    place: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        noEmpty: true,
+      },
+    },
+    start_date: {
+      type: DataType.DATE,
+      allowNull: false,
+      validate: {
+        isDate: true,
+      },
+    },
+    end_date: {
+      type: DataType.DATE,
+      allowNull: false,
+      validate: {
+        isDate: true,
+        isAfterThanStartDate(value) {
+          const startDate = new Date(this.start_date)
+          const endDate = new Date(value)
+          if (startDate <= endDate) {
+            throw new Error("End date must be after start date");
+          }
+        },
+      },
+    },
+  });
+};

8f92b59415faf814aadf0291693c9d4a94819bb3
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-16 02:11:35 -0500

    Add: trips endppoints

---
+++ src/routes/trips.js
@@ -0,0 +1,22 @@
+module.exports = (app) => {
+  const Trips = app.db.models.Trips;
+
+  app
+    .route("/trips")
+    .get((req, res) => {
+      Trips.findAll({})
+        .then((data) => res.json(data))
+        .catch((error) => res.status(422).json({ error: error.message }));
+    })
+    .post((req, res) => {
+      Trips.create(req.body)
+        .then((data) => {
+          console.log(data);
+          return res.json({ status: "received and created" });
+        })
+        .catch((error) => {
+          const errors = error.errors.map((e) => e.message);
+          return res.status(422).json({ errors });
+        });
+    });
+};

b867b79639a5554ffa26ccd4473abb6f0f11eabe
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-18 02:14:44 -0500

    Fix: validator in user model

--- src/models/user.js
+++ src/models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataType) => {
       type: DataType.STRING,
       allowNull: false,
       validate: {
-        noEmpty: true,
+        notEmpty: true,
       },
     },
     username: {

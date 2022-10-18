a5dcad4ca1f5f0dd97b216e53c8156105d9ed1e4
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-18 01:49:50 -0500

    Change: trips route to match model name

--- src/routes/trips.js
+++ src/routes/trips.js
@@ -1,23 +1,23 @@
 module.exports = (app) => {
-  const Trips = app.db.models.Trips;
+  const Trip = app.models.index.models.Trip;
 
   app
     .route("/trips")
     .get((req, res) => {
-      Trips.findAll({})
+      Trip.findAll({})
         .then((data) => res.json(data))
         .catch((error) => res.status(422).json({ error: error.message }));
     })
     .post((req, res) => {
-      Trips.create(req.body)
+      Trip.create(req.body)
         .then((data) => {
           console.log(data);
           return res.json({ status: "received and created" });
         })
         .catch((error) => {
-          console.log(error)
-          const errors = error.errors?.map((e) => e.message);
-          return res.status(422).json({ errors });
+          console.log("ERROR:", error.message);
+          // const errors = error.errors?.map((e) => e.message);
+          return res.status(422).json({ error: error.message });
         });
     });
 };

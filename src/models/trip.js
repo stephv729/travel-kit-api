829ae946cba0cc3a513d4ed38d937915472062e5
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-18 03:25:59 -0500

    Add: association between trips and blogPosts

--- src/models/trip.js
+++ src/models/trip.js
@@ -42,6 +42,7 @@ module.exports = (sequelize, DataType) => {
     classMethods: {
       associate(models) {
         Trip.belongsTo(models.Users)
+        Trip.hasOne(models.BlogPosts)
       },
     },
   });

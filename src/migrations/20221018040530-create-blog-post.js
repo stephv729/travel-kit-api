f999e44c8a09a39a740287f37c4fb8a5b1ee177a
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-18 03:25:29 -0500

    Change:  review model to blogPost model

---
+++ src/migrations/20221018040530-create-blog-post.js
@@ -0,0 +1,42 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("BlogPosts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      body: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      TripId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onDelete: "CASCADE",
+        references: {
+          model: "Trips",
+          key: "id"
+        }
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("BlogPosts");
+  },
+};
---
+++ src/models/blogPost.js
@@ -0,0 +1,61 @@
+// 'use strict';
+// const {
+//   Model
+// } = require('sequelize');
+// module.exports = (sequelize, DataTypes) => {
+//   class Review extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   }
+//   Review.init({
+//     title: DataTypes.STRING,
+//     body: DataTypes.STRING
+//   }, {
+//     sequelize,
+//     modelName: 'Review',
+//   });
+//   return Review;
+// };
+
+
+'use strict';
+module.exports = (sequelize, DataType) => {
+  const Review = sequelize.define("BlogPost", {
+    id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    TripId:{
+      type: DataType.INTEGER,
+      allowNull: false,
+    },
+    Body: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+    Title: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  },{
+    classMethods: {
+      associate(models) {
+        BlogPost.belongsTo(models.Trips)
+      },
+    },
+  });
+  return BlogPost
+};

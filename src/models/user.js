a94feb51ad3c2cb96ee777ec7291db83303ebae9
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-18 01:46:56 -0500

    Add: user model through sequelizer-cli

---
+++ src/models/user.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = (sequelize, DataType) => {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        noEmpty: true,
+      },
+    },
+    username: {
+      type: DataType.STRING,
+      allowNull: true,
+      validate: {
+        len: [5, 10],
+      },
+    },
+    email: {
+      type: DataType.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataType.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true,
+      },
+    },
+  },{
+    classMethods: {
+      associate(models) {
+        User.hasMany(models.Trip,{
+            foreignKey: "UserId", as: "Trip"
+          });
+      },
+    },
+  });
+  return User;
+};

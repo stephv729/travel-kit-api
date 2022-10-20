8d26d65bf642551d675477eb125d75275b2f842a
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-20 10:25:34 -0500

    Add: cors dependecy to enable fetch from client

--- src/libs/middlewares.js
+++ src/libs/middlewares.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 
 module.exports = (app) => {
-  app.set("port", process.env.PORT || 3050);
+  app.set("port", process.env.PORT || 8000);
   app.use(express.json());
+  app.use(cors());
 };

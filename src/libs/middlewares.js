c968f1e59ab1571f1adcdf2cd20eddb07e757bd9
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-21 12:15:49 -0500

    Change: verification to also check match the current user token

--- src/libs/middlewares.js
+++ src/libs/middlewares.js
@@ -3,34 +3,35 @@ import cors from "cors";
 import { tokenKey } from "../keys";
 import jwt from "jsonwebtoken";
 
-const verification = express.Router();
-verification.use((req, res, next) => {
-  let token = req.headers["authorization"] || req.headers["x-access-token"];
-  const tokenType = "Bearer ";
-  if (!token) {
-    return res.status(401).send({
-      error: "Must sign up or login",
-    });
-  }
-  if (token.startsWith(tokenType)) {
-    token = token.slice(tokenType.length, token.length);
-    console.log(token);
-  }
-  if (token) {
-    jwt.verify(token, tokenKey, (error, decoded) => {
-      if (error) {
-        return res.json({
-          msg: "Invalid token",
-        });
-      } else {
-        req.decoded = decoded;
-        next();
-      }
-    });
-  }
-});
-
 module.exports = (app) => {
+  const verification = express.Router();
+  verification.use((req, res, next) => {
+    let token = req.headers["authorization"] || req.headers["x-access-token"];
+    const tokenType = "Bearer ";
+    if (!token) {
+      return res.status(401).send({
+        error: "Must sign up or login",
+      });
+    }
+    if (token.startsWith(tokenType)) {
+      token = token.slice(tokenType.length, token.length);
+    }
+    if (token) {
+      jwt.verify(token, tokenKey, (error, decoded) => {
+        const currentUser = app.get("user");
+        console.log(currentUser);
+        if (error || token !== currentUser?.dataValues?.token) {
+          return res.json({
+            error: "Invalid token",
+          });
+        } else {
+          req.decoded = decoded;
+          next();
+        }
+      });
+    }
+  });
+
   app.set("port", process.env.PORT || 8000);
   app.set("key", tokenKey);
   app.set("jwt", jwt);

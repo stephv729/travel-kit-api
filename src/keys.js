0d6258c6d90456baf44f8eb65a0c5c50369ee91d
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-21 09:42:52 -0500

    Add: verification middleware

---
+++ src/keys.js
@@ -0,0 +1,1 @@
+export const tokenKey = "travel-kit-api-token";
--- src/libs/middlewares.js
+++ src/libs/middlewares.js
@@ -1,8 +1,42 @@
 import express from "express";
 import cors from "cors";
+import { tokenKey } from "../keys";
+import jwt from "jsonwebtoken";
+
+const verification = express.Router();
+verification.use((req, res, next) => {
+  let token = req.headers["authorization"] || req.headers["x-access-token"];
+  const tokenType = "Bearer ";
+  if (!token) {
+    return res.status(401).send({
+      error: "Must sign up or login",
+    });
+  }
+  if (token.startsWith(tokenType)) {
+    token = token.slice(tokenType.length, token.length);
+    console.log(token);
+  }
+  if (token) {
+    jwt.verify(token, tokenKey, (error, decoded) => {
+      if (error) {
+        return res.json({
+          msg: "Invalid token",
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  }
+});
 
 module.exports = (app) => {
   app.set("port", process.env.PORT || 8000);
+  app.set("key", tokenKey);
+  app.set("jwt", jwt);
+  app.set("verification", verification);
+  app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
   app.use(cors());
 };
+// const jwt = require("jsonwebtoken");

5321f55c5f2a245bb18c66e9327f9fc32e912ff4
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-21 12:14:33 -0500

    Add: token column to users model

--- src/models/user.js
+++ src/models/user.js
@@ -1,48 +1,57 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataType) => {
-  const User = sequelize.define("User", {
-    id: {
-      type: DataType.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataType.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
+  const User = sequelize.define(
+    "User",
+    {
+      id: {
+        type: DataType.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataType.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      username: {
+        type: DataType.STRING,
+        allowNull: true,
+        validate: {
+          len: [5, 20],
+        },
+      },
+      email: {
+        type: DataType.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+          isEmail: true,
+        },
+      },
+      password: {
+        type: DataType.STRING,
+        allowNull: false,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      token: {
+        type: DataType.STRING,
+        allowNull: true,
       },
     },
-    username: {
-      type: DataType.STRING,
-      allowNull: true,
-      validate: {
-        len: [5, 10],
-      },
-    },
-    email: {
-      type: DataType.STRING,
-      unique: true,
-      allowNull: false,
-      validate: {
-        isEmail: true,
-      },
-    },
-    password: {
-      type: DataType.STRING,
-      allowNull: false,
-      validate: {
-        notEmpty: true,
-      },
-    },
-  },{
-    classMethods: {
-      associate(models) {
-        User.hasMany(models.Trip,{
-            foreignKey: "UserId", as: "Trip"
+    {
+      classMethods: {
+        associate(models) {
+          User.hasMany(models.Trip, {
+            foreignKey: "UserId",
+            as: "Trip",
           });
+        },
       },
-    },
-  });
+    }
+  );
   return User;
 };

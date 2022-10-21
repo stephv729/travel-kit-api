386f739577a8e15ba99fb0d854f6915408a421a8
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-21 14:58:49 -0500

    Add: profile route for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,27 +1,43 @@
 module.exports = (app) => {
   const User = app.models.index.models.User;
+  const verification = app.get("verification");
+
+  app.route("/users").get((req, res) => {
+    User.findAll({})
+      .then((data) => res.json(data))
+      .catch((error) => res.status(422).json({ error: error.message }));
+  });
+
+  app.route("/users/:id").get((req, res) => {
+    User.findOne({ where: req.params })
+      .then((user) => {
+        const { createdAt, updatedAt, token, ...userData } = user.dataValues;
+        res.json(userData);
+      })
+      .catch((error) => res.status(422).json({ error: error.message }));
+  });
 
   app
-    .route("/users")
-    .get((req, res) => {
-      User.findAll({})
-        .then((data) => res.json(data))
-        .catch((error) => res.status(422).json({ error: error.message }));
-    })
-
-  app
-    .route("/users/:id")
-    .get((req, res) => {
-      User.findOne({ where: req.params })
+    .route("/profile")
+    .get(verification, (req, res) => {
+      const currentUser = app.get("user");
+      if (!currentUser) {
+        return res.json({ error: "Must Signup or Login first" });
+      }
+      User.findOne({ where: { id: currentUser.dataValues.id } })
         .then((user) => {
-          const { createdAt, updatedAt, token,...userData } = user.dataValues;
-          res.json(userData)
+          const { createdAt, updatedAt, token, ...userData } = user.dataValues;
+          res.json(userData);
         })
         .catch((error) => res.status(422).json({ error: error.message }));
     })
     .patch((req, res) => {
-      User.update(req.body, { where: req.params })
-        .then((data) => res.json({ status: "updated" }))
+      const currentUser = app.get("user");
+      if (!currentUser) {
+        return res.json({ error: "Must Signup or Login first" });
+      }
+      User.update(req.body, { where: { id: currentUser.dataValues.id } })
+        .then((_data) => res.json({ status: "updated" }))
         .catch((error) => res.status(422).json({ error: error.message }));
     });
 };

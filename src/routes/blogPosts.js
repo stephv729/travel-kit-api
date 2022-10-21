fdc3aadf00e5ab8068a6712d341259e4b2e58578
Stephanny Vargas <91625172+stephv729@users.noreply.github.com>
2022-10-20 20:44:39 -0500

    Change: blogPost response in show method

--- src/routes/blogPosts.js
+++ src/routes/blogPosts.js
@@ -1,5 +1,17 @@
 module.exports = (app) => {
   const BlogPost = app.models.index.models.BlogPost;
+  const Trip = app.models.index.models.Trip;
+  const User = app.models.index.models.User;
+
+  const findTrip = (bg) => {
+    const tripId = bg.dataValues["TripId"];
+    return Trip.findOne({ where: { id: tripId } });
+  };
+
+  const findUser = (trip) => {
+    const userId = trip.dataValues["UserId"];
+    return User.findOne({ where: { id: userId } });
+  };
 
   app
     .route("/blogposts")
@@ -10,9 +22,7 @@ module.exports = (app) => {
     })
     .post((req, res) => {
       BlogPost.create(req.body)
-        .then((data) => {
-          return res.json(data);
-        })
+        .then((data) => res.json(data))
         .catch((error) => {
           console.log("ERROR:", error.message);
           const errors = error.errors?.map((e) => e.message);
@@ -24,10 +34,19 @@ module.exports = (app) => {
 
   app
     .route("/blogposts/:id")
-    .get((req, res) => {
-      BlogPost.findOne({ where: req.params })
-        .then((data) => res.json(data))
-        .catch((error) => res.status(422).json({ error: error.message }));
+    .get(async (req, res) => {
+      try {
+        const blogPost = await BlogPost.findOne({ where: req.params });
+        const trip = await findTrip(blogPost);
+        const user = await findUser(trip);
+        const { createdAt, updatedAt, ...userData } = user.dataValues;
+        return res.json({
+          ...blogPost.dataValues,
+          userDetails: userData,
+        });
+      } catch {
+        (error) => res.status(422).json({ error: error.message });
+      }
     })
     .delete((req, res) => {
       BlogPost.destroy({ where: req.params })
